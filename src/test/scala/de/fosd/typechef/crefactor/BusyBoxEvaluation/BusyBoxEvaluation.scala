b49ecc767ebfc530aa3160cbd3e741fb118f8fe5
aJanker <janker@fim.uni-passau.de>
2013-06-19 21:10:02 +0200

    RunExec

--- src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/BusyBoxEvaluation.scala
+++ src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/BusyBoxEvaluation.scala
@@ -20,29 +20,64 @@ trait BusyBoxEvaluation extends EvalHelper {
 
 object RefactorVerification extends EvalHelper {
 
-    def copyFile(file1: File, file2: File) = {
-        val src = file1
-        val dest = file2
-        new FileOutputStream(dest) getChannel() transferFrom(new FileInputStream(src) getChannel, 0, Long.MaxValue)
-        println(dest.length())
-    }
+    def copyFile(file1: File, file2: File) = new FileOutputStream(file2) getChannel() transferFrom(new FileInputStream(file1) getChannel, 0, Long.MaxValue)
 
     def verify(bbFile: File, run: Int, fm: FeatureModel): Boolean = {
         val verfiyPath = bbFile.getCanonicalPath
         val orgFile = new File(bbFile.getCanonicalPath.replaceAll("busybox-1.18.5", "busybox-1.18.5_untouched"))
         val refFile = new File(bbFile.getCanonicalPath.replaceAll("busybox-1.18.5", "result") + "/" + run + "/" + bbFile.getName)
+        val verfiyDir = new File(bbFile.getCanonicalPath.replaceAll("busybox-1.18.5", "result") + "/" + run + "/")
 
-        buildBusyBox
-        println(bbFile.length())
-        bbFile.delete()
+        val configs = verfiyDir.listFiles(new FilenameFilter {
+            def accept(input: File, file: String): Boolean = file.endsWith(".config")
+        })
 
-        copyFile(refFile, new File(verfiyPath))
+        configs.forall(config => {
+            val configBuild = new File(busyBoxPath + ".config")
+            copyFile(config, configBuild)
 
-        // TODO Config
-        // First Build normal busybox
-        buildBusyBox
-        // TODO Test
-        true
+            buildBusyBox
+            val org = runTest
+            bbFile.delete()
+            config.delete()
+
+            val buildRefFile = new File(verfiyPath)
+            copyFile(refFile, buildRefFile)
+
+            buildBusyBox
+            val ref = runTest
+            buildRefFile.delete()
+            copyFile(orgFile, new File(verfiyPath))
+
+            configBuild.delete()
+            println("Result" + org.equals(ref))
+            true
+        })
+    }
+
+    def runTest: String = {
+        var error = false
+        val pb = new ProcessBuilder("./runtest.sh")
+        pb.directory(new File(busyBoxPath + "testsuite/"))
+        val p = pb.start()
+        p.waitFor()
+
+        val reader = new BufferedReader(new InputStreamReader(p.getInputStream()))
+        val sb = new StringBuilder
+        while (reader.ready()) {
+            val line = reader.readLine()
+            sb.append(line)
+            println(line)
+        }
+
+        val reader2 = new BufferedReader(new InputStreamReader(p.getErrorStream()))
+        while (reader2.ready()) {
+            error = true
+            val line = reader2.readLine()
+            sb.append(line)
+            println(line)
+        }
+        sb.toString()
     }
 
     def buildBusyBox: Boolean = {
