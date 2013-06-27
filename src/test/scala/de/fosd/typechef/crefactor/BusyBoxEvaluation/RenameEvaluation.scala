2c923640786e5ea945f86d4a674eef0d09eea3c6
ajanker <an.janker@googlemail.com>
2013-06-27 19:10:48 +0200

    StackTrace toString

--- src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
+++ src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
@@ -36,7 +36,7 @@ class RenameEvaluation extends BusyBoxEvaluation {
                 verify
             } catch {
                 case e: Exception => {
-                    writeError(e.getCause + "\n" + e.getStackTrace, bb_file.getCanonicalPath, 0)
+                    writeError(e.getCause + "\n" + e.getStackTrace.toString, bb_file.getCanonicalPath, 0)
                     false
                 }
             }
