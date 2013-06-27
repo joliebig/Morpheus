d4cb76a825e71dbb5862657c22b95eef0a2d4d77
ajanker <an.janker@googlemail.com>
2013-06-27 15:49:55 +0200

    Optimized BusyBox Building

--- src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/BusyBoxEvaluation.scala
+++ src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/BusyBoxEvaluation.scala
@@ -23,7 +23,7 @@ object RefactorVerification extends EvalHelper {
     def copyFile(file1: File, file2: File) = new FileOutputStream(file2).getChannel() transferFrom(new FileInputStream(file1).getChannel, 0, Long.MaxValue)
 
     def verify(bbFile: File, run: Int, fm: FeatureModel): Boolean = {
-        val verfiyPath = bbFile.getCanonicalPath
+        val workingPath = bbFile.getCanonicalPath
         val orgFile = new File(bbFile.getCanonicalPath.replaceAll("busybox-1.18.5", "busybox-1.18.5_untouched"))
         val refFile = new File(bbFile.getCanonicalPath.replaceAll("busybox-1.18.5", "result") + "/" + run + "/" + bbFile.getName)
         val verfiyDir = new File(bbFile.getCanonicalPath.replaceAll("busybox-1.18.5", "result") + "/" + run + "/")
@@ -33,28 +33,33 @@ object RefactorVerification extends EvalHelper {
         })
 
         configs.forall(config => {
+            def buildAndTest(busyBoxFile: File, ext: String): String = {
+                val buildResult = buildBusyBox
+                val testResult = runTest
+                writeResult(buildResult._2, verfiyDir.getCanonicalPath + "/" + config.getName + ext + ".buildAndTest")
+                writeResult(testResult, verfiyDir.getCanonicalPath + "/" + config.getName + ext + ".test")
+                bbFile.delete()
+                testResult
+            }
+
             val configBuild = new File(busyBoxPath + ".config")
             copyFile(config, configBuild)
 
-            val orgBuild = buildBusyBox
-            val org = runTest
-            writeResult(orgBuild._1, verfiyDir.getCanonicalPath + "/" + config.getName + "_org" + ".build")
-            writeResult(org, verfiyDir.getCanonicalPath + "/" + config.getName + "_org" + ".test")
-            bbFile.delete()
+            val orgTest = buildAndTest(bbFile, "_org")
 
-            val buildRefFile = new File(verfiyPath)
+            val buildRefFile = new File(workingPath)
+
+            // Replace original file with refactored file
             copyFile(refFile, buildRefFile)
 
-            val refBuild = buildBusyBox
-            val ref = runTest
-            writeResult(refBuild._1, verfiyDir.getCanonicalPath + "/" + config.getName + "_ref" + ".build")
-            writeResult(ref, verfiyDir.getCanonicalPath + "/" + config.getName + "_ref" + ".test")
-            buildRefFile.delete()
-            copyFile(orgFile, new File(verfiyPath))
+            val refTest = buildAndTest(buildRefFile, "_ref")
 
+            // Restore old original file again
+            copyFile(orgFile, new File(workingPath))
             configBuild.delete()
-            println("Result " + org.equals(ref))
-            org.equals(ref)
+
+            writeResult(orgTest.equals(refTest).toString, verfiyDir.getCanonicalPath + "/" + config.getName + ".result")
+            orgTest.equals(refTest)
         })
     }
 
@@ -93,29 +98,26 @@ object RefactorVerification extends EvalHelper {
         sb.toString()
     }
 
-    def buildBusyBox: (String, String) = {
-        var error = false
+    def buildBusyBox: (Boolean, String, String) = {
+        def readIn(reader: BufferedReader, builder: StringBuilder): String = {
+            while (reader.ready()) {
+                val line = reader.readLine()
+                builder.append(line)
+            }
+            builder.toString()
+        }
+
         val pb = new ProcessBuilder("./buildBusyBox.sh")
         pb.directory(new File(busyBoxPath))
         val p = pb.start()
         p.waitFor()
 
-        val reader = new BufferedReader(new InputStreamReader(p.getInputStream))
-        val builderOut = new StringBuilder
-        while (reader.ready()) {
-            val line = reader.readLine()
-            builderOut.append(line)
-            println(line)
-        }
+        val readerOut = new BufferedReader(new InputStreamReader(p.getInputStream))
+        val readerErr = new BufferedReader(new InputStreamReader(p.getErrorStream))
 
-        val builderErr = new StringBuilder
-        val reader2 = new BufferedReader(new InputStreamReader(p.getErrorStream))
-        while (reader2.ready()) {
-            error = true
-            val line = reader2.readLine()
-            builderErr.append(line)
-            println(line)
-        }
-        (builderOut.toString(), builderErr.toString())
+        val out: String = readIn(readerOut, new StringBuilder)
+        val err: String = readIn(readerErr, new StringBuilder)
+
+        (out.contains("Success_Build"), out, err)
     }
 }
