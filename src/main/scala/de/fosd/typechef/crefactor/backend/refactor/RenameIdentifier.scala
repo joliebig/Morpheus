8326f63ddfad96ff8f26f0518c85666ae7b53354
ajanker <an.janker@googlemail.com>
2013-07-03 17:40:16 +0200

    error file if test fails

--- src/main/scala/de/fosd/typechef/crefactor/backend/refactor/RenameIdentifier.scala
+++ src/main/scala/de/fosd/typechef/crefactor/backend/refactor/RenameIdentifier.scala
@@ -18,13 +18,12 @@ object RenameIdentifier extends ASTSelection with Refactor {
     def isAvailable(morpheus: Morpheus, selection: Selection): Boolean = !getAvailableIdentifiers(morpheus, selection).isEmpty
 
     def rename(id: Id, newName: String, morpheus: Morpheus): AST = {
-        return morpheus.getAST
-        /*assert(isValidName(newName), Configuration.getInstance().getConfig("default.error.invalidName"))
+        assert(isValidName(newName), Configuration.getInstance().getConfig("default.error.invalidName"))
         // TODO Optimize Performance by avoiding traversing the ast for each element
         getAllConnectedIdentifier(id, morpheus.getDeclUseMap, morpheus.getUseDeclMap).foldLeft(morpheus.getAST)((ast, id) => {
             assert(new File(id.getFile.get.replaceFirst("file ", "")).canWrite, "Not writeable: " + id.getPositionFrom.getFile)
             assert(!isShadowed(newName, id, morpheus), Configuration.getInstance().getConfig("refactor.rename.failed.shadowing"))
             replaceInAST(ast, id, id.copy(name = newName))
-        }) */
+        })
     }
 }
