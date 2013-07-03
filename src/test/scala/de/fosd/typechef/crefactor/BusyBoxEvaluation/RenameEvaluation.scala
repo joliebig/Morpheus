02318603022cfddd02678587f6cb349ddac01250
ajanker <an.janker@googlemail.com>
2013-07-03 21:21:49 +0200

    Run Refactor more often

--- src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
+++ src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/RenameEvaluation.scala
@@ -3,7 +3,7 @@ package de.fosd.typechef.crefactor.BusyBoxEvaluation
 import org.junit.Test
 import java.io.File
 import de.fosd.typechef.parser.c.{Id, AST}
-import de.fosd.typechef.featureexpr.FeatureExpr
+import de.fosd.typechef.featureexpr.{FeatureModel, FeatureExpr}
 import de.fosd.typechef.crefactor.util.{PrepareRefactoredASTforEval, TimeMeasurement}
 import de.fosd.typechef.crefactor.Morpheus
 import de.fosd.typechef.crefactor.backend.refactor.RenameIdentifier
@@ -12,9 +12,37 @@ class RenameEvaluation extends BusyBoxEvaluation {
 
     private val REFACTOR_NAME = "refactoredID"
 
+    private val MAX = 3
+
 
     @Test
     def evaluate() {
+        def runRefactor(morpheus: Morpheus, stats: List[Any], bb_file: File, fm: FeatureModel, run: Int, max: Int, lastResult: Boolean = true): Boolean = {
+            if (run >= max) return lastResult
+            try {
+                val result = applyRefactor(morpheus, stats)
+                if (result._2) {
+                    val dir = getResultDir(bb_file.getCanonicalPath, run)
+                    val path = dir.getCanonicalPath + File.separatorChar + getFileName(bb_file.getCanonicalPath)
+                    writeAST(result._1, path)
+                    PrepareRefactoredASTforEval.makeConfigs(result._1, morpheus.getFeatureModel, bb_file.getCanonicalPath, result._3, run)
+                }
+
+                val verify = Verification.verify(bb_file, run, fm)
+                var stat2 = result._4
+                stat2 = stat2.::(result._2 + "\n" + verify)
+                writeStats(stat2, bb_file.getCanonicalPath, run)
+                verify && runRefactor(morpheus, stats, bb_file, fm, run + 1, MAX)
+            } catch {
+                case e: Exception => {
+                    println(e.getMessage)
+                    println(e.getStackTrace.mkString("\n"))
+                    writeExeception(e.getMessage + "\n" + e.getStackTrace.mkString("\n"), bb_file.getCanonicalPath, run)
+                    false
+                }
+            }
+        }
+
         val files = getBusyBoxFiles.reverse
         val refactor = files.map(file => {
             val bb_file = new File(busyBoxPath + file)
@@ -27,19 +55,7 @@ class RenameEvaluation extends BusyBoxEvaluation {
                 val morpheus = new Morpheus(ast, fm)
                 val parseTypeCheckTime = parseTypeCheckMs.getTime
                 stats ::= parseTypeCheckTime
-                val result = applyRefactor(morpheus, stats)
-                if (result._2) {
-                    val dir = getResultDir(bb_file.getCanonicalPath, 0)
-                    val path = dir.getCanonicalPath + File.separatorChar + getFileName(bb_file.getCanonicalPath)
-                    writeAST(result._1, path)
-                    PrepareRefactoredASTforEval.makeConfigs(result._1, morpheus.getFeatureModel, bb_file.getCanonicalPath, result._3, 0)
-                }
-
-                val verify = Verification.verify(bb_file, 0, fm)
-                var stat2 = result._4
-                stat2 = stat2.::(result._2 + "\n" + verify)
-                writeStats(stat2, bb_file.getCanonicalPath, 0)
-                verify
+                runRefactor(morpheus, stats, bb_file, fm, 0, MAX)
             } catch {
                 case e: Exception => {
                     println(e.getMessage)
