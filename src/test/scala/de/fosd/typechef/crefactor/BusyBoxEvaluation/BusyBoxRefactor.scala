463e0a8beff78895219c712383aea906eb18b875
ajanker <an.janker@googlemail.com>
2013-07-26 11:22:26 +0200

    Changed test start signature because of some wired null pointers

--- src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/BusyBoxRefactor.scala
+++ src/test/scala/de/fosd/typechef/crefactor/BusyBoxEvaluation/BusyBoxRefactor.scala
@@ -8,32 +8,31 @@ import de.fosd.typechef.crefactor.Morpheus
 trait BusyBoxRefactor extends BusyBoxEvaluation with Refactor {
 
     @Test
-    def evaluate() {
-        val files = getBusyBoxFiles.reverse
-        val refactor = files.map(file => {
-            val bb_file = new File(busyBoxPath + file)
-            try {
-                var stats = List[Any]()
-                val parseTypeCheckMs = new TimeMeasurement
-                val parsed = parse(bb_file)
-                val ast = parsed._1
-                val fm = parsed._2
-                val morpheus = new Morpheus(ast, fm)
-                val parseTypeCheckTime = parseTypeCheckMs.getTime
-                stats ::= parseTypeCheckTime
-                runRefactor(morpheus, stats, bb_file, fm, 0, MAX)
-            } catch {
-                case e: Exception => {
-                    println(e.getCause.toString)
-                    println(e.getMessage)
-                    println(e.getStackTrace.mkString("\n"))
-                    writeExeception(e.getCause.toString + "\n" + e.getMessage + "\n" + e.getStackTrace.mkString("\n"), bb_file.getCanonicalPath, 0)
-                    false
-                }
+    def evaluate(file: String) {
+        // val files = getBusyBoxFiles.reverse
+        val bb_file = new File(busyBoxPath + file)
+        var ref_result: Boolean = false
+        try {
+            var stats = List[Any]()
+            val parseTypeCheckMs = new TimeMeasurement
+            val parsed = parse(bb_file)
+            val ast = parsed._1
+            val fm = parsed._2
+            val morpheus = new Morpheus(ast, fm)
+            val parseTypeCheckTime = parseTypeCheckMs.getTime
+            stats ::= parseTypeCheckTime
+            ref_result = runRefactor(morpheus, stats, bb_file, fm, 0, MAX)
+        } catch {
+            case e: Exception => {
+                println(e.getCause.toString)
+                println(e.getMessage)
+                println(e.getStackTrace.mkString("\n"))
+                writeExeception(e.getCause.toString + "\n" + e.getMessage + "\n" + e.getStackTrace.mkString("\n"), bb_file.getCanonicalPath, 0)
+                ref_result = false
             }
-        })
-        println("Refactor succ: " + refactor.contains(false))
-        refactor.contains(false)
+        }
+        println("Refactor succ: " + result)
+        ref_result
     }
 
 }
