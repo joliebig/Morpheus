6929e25dbd5f90eae0ff4aaaff48f3266d1062f1
ajanker <an.janker@googlemail.com>
2013-08-01 09:32:00 +0200

    Short Debug for failed tests

--- src/main/scala/de/fosd/typechef/crefactor/backend/refactor/CRenameIdentifier.scala
+++ src/main/scala/de/fosd/typechef/crefactor/backend/refactor/CRenameIdentifier.scala
@@ -4,8 +4,6 @@ import de.fosd.typechef.crefactor.backend.ASTSelection
 import de.fosd.typechef.parser.c.{Id, AST}
 import de.fosd.typechef.crefactor.frontend.util.Selection
 import de.fosd.typechef.crefactor.Morpheus
-import java.io.File
-import de.fosd.typechef.crefactor.evaluation_utils.Configuration
 
 /**
  * Implements the technique of correctly renaming an identifier.
@@ -20,12 +18,13 @@ object CRenameIdentifier extends ASTSelection with CRefactor {
     def isAvailable(morpheus: Morpheus, selection: Selection): Boolean = !getAvailableIdentifiers(morpheus, selection).isEmpty
 
     def rename(id: Id, newName: String, morpheus: Morpheus): AST = {
-        assert(isValidName(newName), Configuration.getInstance().getConfig("default.error.invalidName"))
+        morpheus.getAST
+        /*assert(isValidName(newName), Configuration.getInstance().getConfig("default.error.invalidName"))
         // TODO Optimize Performance by avoiding traversing the ast for each element
         getAllConnectedIdentifier(id, morpheus.getDeclUseMap, morpheus.getUseDeclMap).foldLeft(morpheus.getAST)((ast, id) => {
             assert(new File(id.getFile.get.replaceFirst("file ", "")).canWrite, "Not writeable: " + id.getPositionFrom.getFile)
             assert(!isShadowed(newName, id, morpheus), Configuration.getInstance().getConfig("refactor.rename.failed.shadowing"))
             replaceInAST(ast, id, id.copy(name = newName))
-        })
+        }) */
     }
 }
