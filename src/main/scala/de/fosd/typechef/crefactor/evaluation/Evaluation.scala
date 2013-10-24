47659b06ac6e10786c370784ac25e1421d8b71b5
aJanker <janker@fim.uni-passau.de>
2013-10-24 18:57:27 +0200

    - BugFix for not writing prettyprinted ast

--- src/main/scala/de/fosd/typechef/crefactor/evaluation/Evaluation.scala
+++ src/main/scala/de/fosd/typechef/crefactor/evaluation/Evaluation.scala
@@ -310,12 +310,12 @@ trait Evaluation extends Logging {
     def writeAST(ast: AST, filePath: String) {
         println("PrettyPrint AST to: " + filePath)
         val file = new File(filePath)
-        if (file.exists) file.delete
         val prettyPrinted = PrettyPrinter.print(ast)
         val writer = new FileWriter(file)
         writer.write(prettyPrinted.replaceAll("definedEx", "defined"))
         writer.flush()
         writer.close()
+        println(prettyPrinted.length)
     }
 
     def writePlainAST(ast: AST, filePath: String) {
