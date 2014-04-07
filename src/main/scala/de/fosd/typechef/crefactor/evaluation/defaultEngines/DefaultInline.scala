74819a3b3ed5f01fd6316692c300c23ce373389e
aJanker <cGH!igxhb9>
2014-04-07 13:02:06 +0200

    Added clean option for keeping or removing function declarations.

--- src/main/scala/de/fosd/typechef/crefactor/evaluation/defaultEngines/DefaultInline.scala
+++ src/main/scala/de/fosd/typechef/crefactor/evaluation/defaultEngines/DefaultInline.scala
@@ -39,7 +39,7 @@ trait DefaultInline extends Refactoring with Evaluation {
 
         try {
             val refTime = new StopClock
-            val refAST = CInlineFunction.inline(morpheus, callIdToInline, true)
+            val refAST = CInlineFunction.inline(morpheus, callIdToInline, true, true)
 
             refAST match {
                 case Left(errmsg) => {
