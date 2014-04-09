5b937467c09a4f40281ed89812672ae8c38fc35d
aJanker <cGH!igxhb9>
2014-04-09 17:17:53 +0200

    Added SQLite Interface generator.

---
+++ src/main/scala/de/fosd/typechef/crefactor/evaluation/evalcases/sqlite/setup/SQLiteInterfaceGenerator.scala
@@ -0,0 +1,7 @@
+package de.fosd.typechef.crefactor.evaluation.evalcases.sqlite.setup
+
+import de.fosd.typechef.crefactor.evaluation.evalcases.sqlite.SQLiteEvaluation
+import de.fosd.typechef.crefactor.evaluation.setup.CLinkingInterfaceGenerator
+
+
+object SQLiteInterfaceGenerator extends SQLiteEvaluation with CLinkingInterfaceGenerator{}
