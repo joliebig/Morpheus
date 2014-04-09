e33103efc162692c59e9f7712634f8dbff6cf713
aJanker <cGH!igxhb9>
2014-04-09 16:11:44 +0200

    finishing sqlite config generation and code refactor

--- src/main/scala/de/fosd/typechef/crefactor/evaluation/evalcases/sqlite/SQLiteVerification.scala
+++ src/main/scala/de/fosd/typechef/crefactor/evaluation/evalcases/sqlite/SQLiteVerification.scala
@@ -12,39 +12,9 @@ object SQLiteVerification extends SQLiteEvaluation with Verification {
         // not supported
     }
 
-    override def completeVerify(evalFile: String, fm: FeatureModel, affectedFeatures: List[FeatureExpr] = List()) = {
-        val resultDir = new File(evalFile.replaceAll(evalName, "result") + "/")
-        if (!resultDir.exists)
-            resultDir.mkdirs
-
-        val confFeatures = new ConfigFeatures(allFeatures._1)
-
-        // get features
-        val featureCombinations = getFeatureCombinations(confFeatures, affectedFeatures)
-
-        // ref first
-        // default (build + test)
-        configure()
-        val defRef = buildAndTest(resultDir, -1, "_ref")
-        logger.info("Can build and test " + evalFile + " in def config and ref: " + defRef)
-        // feature combos
-
-        // clean up the refactor mess
-        runScript(cleanScript, sourcePath)
-
-        //org second
-        // default (build + test)
-        configure()
-        val defOrg = buildAndTest(resultDir, -1, "_org")
-        logger.info("Can build and test " + evalFile + " in def config and org: " + defOrg)
-        // feature combos
-
-        runScript(cleanScript, sourcePath)
-
+    override def configure(configuration: SimpleConfiguration) = {
+       false
     }
-
-    override def configure(configuration: SimpleConfiguration) = false
-    override def configure() = false
 
 
 }
