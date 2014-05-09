ed46ba77d89a5f0591eb23fdd5a847bb6a4a0386
ajanker <an.janker@googlemail.com>
2014-05-09 11:19:53 +0200

    Implemented corresponding statement detection in tunit.

--- src/main/scala/de/fosd/typechef/crefactor/evaluation/Refactor.scala
+++ src/main/scala/de/fosd/typechef/crefactor/evaluation/Refactor.scala
@@ -2,13 +2,12 @@ package de.fosd.typechef.crefactor.evaluation
 
 import de.fosd.typechef.featureexpr.{FeatureExpr, FeatureModel}
 import de.fosd.typechef.parser.c._
-import de.fosd.typechef.crefactor.Morpheus
+import de.fosd.typechef.crefactor.{Logging, Morpheus}
 import de.fosd.typechef.crefactor.backend.CModuleInterface
 import java.io.{ObjectOutputStream, FileOutputStream}
 import java.util.zip.GZIPOutputStream
 import de.fosd.typechef.parser.c.TranslationUnit
 import de.fosd.typechef.parser.c.Id
-import de.fosd.typechef.crefactor.evaluation.PreparedRefactorings
 
 trait Refactoring {
 
@@ -46,12 +45,41 @@ trait Refactor extends Evaluation {
 }
 
 case class PreparedRefactorings(renaming : List[Id], extract : List[List[Statement]], inline: List[Id])
-    extends Serializable with ASTNavigation with ConditionalNavigation {
+    extends Serializable with ASTNavigation with ConditionalNavigation with Logging {
 
     def getCorrespondingId(id: Id, morpheus : Morpheus) : Option[Id] =
         filterASTElems[Id](morpheus.getTranslationUnit, morpheus.getASTEnv).par.find(tunitId =>
             (tunitId.name == id.name)
                 && tunitId.getPositionFrom.equals(id.getPositionFrom) && tunitId.getPositionTo.equals(id.getPositionTo))
+    /**
+     * Statements have no position range. We filter for an id part of the selection and "bubble up" to find the
+     * corresponding statement in the current tunit.
+     */
+    def getCorrespondingStmts(stmts : List[Statement], morpheus : Morpheus) : Option[List[Statement]] = {
+
+        def findPrirorStatement(a : Product, statement : Statement) : Option[Statement] = {
+            findPriorASTElem[Statement](a, morpheus.getASTEnv) match {
+                case Some(corStmt) =>
+                    if (corStmt.equals(statement)) Some(corStmt)
+                    else findPrirorStatement(corStmt, statement)
+                case None => None
+            }
+        }
+
+        val correspondingStmts = stmts.map(stmt => {
+            val stmtIds = filterASTElems[Id](stmt)
+            if (stmtIds.isEmpty) {
+                logger.error("Could not determine corresponding tunit statement")
+                None
+            } else {
+                val tUnitId = getCorrespondingId(stmtIds.head, morpheus)
+                findPrirorStatement(tUnitId, stmt)
+            }
+        })
+
+        if (correspondingStmts.exists(_ == None)) None
+        else Some(correspondingStmts.map(_.get))
+    }
 
 
 
