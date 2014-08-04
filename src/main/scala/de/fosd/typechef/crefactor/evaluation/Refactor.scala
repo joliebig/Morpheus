7a217977caba897bfe6b00672eb5e6074ef7a63f
Andreas Janker <an.janker@googlemail.com>
2014-08-04 17:21:51 +0200

    sqlite rename only

--- src/main/scala/de/fosd/typechef/crefactor/evaluation/Refactor.scala
+++ src/main/scala/de/fosd/typechef/crefactor/evaluation/Refactor.scala
@@ -23,8 +23,8 @@ trait Refactor extends Evaluation {
                              file: String, linkInterface: CModuleInterface = null) : PreparedRefactorings = {
         val morpheus = new Morpheus(tunit, fm, linkInterface, file)
         val renameIDs = renameEngine.getValidIdsForEvaluation(morpheus)
-        val extractStmts = extractEngine.getValidStatementsForEvaluation(morpheus)
-        val inlineIDs = inlineEngine.getValidIdsForEvaluation(morpheus)
+        val extractStmts = List()
+        val inlineIDs = List()
         logger.info("+++ Prepared refactorings for file " + morpheus.getFile)
         logger.info("Renaming: " + renameIDs.size)
         logger.info("Extract: " + extractStmts.size)
