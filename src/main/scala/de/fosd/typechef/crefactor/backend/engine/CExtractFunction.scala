cf6aa9ced8e71b93a254b38e741aecef49d6b094
Andreas Janker <an.janker@googlemail.com>
2014-08-07 15:33:11 +0200

    Code clean up

--- src/main/scala/de/fosd/typechef/crefactor/backend/engine/CExtractFunction.scala
+++ src/main/scala/de/fosd/typechef/crefactor/backend/engine/CExtractFunction.scala
@@ -236,11 +236,7 @@ object CExtractFunction extends CRefactor with IntraCFG {
     private def hasInvisibleEnumerations(selection: CExtractSelection, morpheus: Morpheus): Boolean = {
         val invisibleEnums = selection.liveIds.exists(liveId => {
             try {
-                val scope = findPriorASTElem[CompoundStatement](liveId, morpheus.getASTEnv) match {
-                    case Some(x) => x.innerStatements.last.entry
-                    case _ => morpheus.getTranslationUnit.defs.last.entry
-                }
-                val enums = ConditionalLib.leaves(morpheus.getEnv(scope).varEnv.lookup(liveId.name))
+                val enums = ConditionalLib.leaves(morpheus.getScopeEnv(liveId).varEnv.lookup(liveId.name))
                 val res = enums.exists {case (_, KEnumVar, 1, _) => true; case _ => false}
 
                 if (res)
@@ -504,11 +500,7 @@ object CExtractFunction extends CRefactor with IntraCFG {
         liveParamIds.foreach(liveId =>
             try {
                 // only lookup variables
-                val scope = findPriorASTElem[CompoundStatement](liveId, morpheus.getASTEnv) match {
-                    case Some(x) => x.innerStatements.last.entry
-                    case _ => morpheus.getTranslationUnit.defs.last.entry
-                }
-                morpheus.getEnv(scope).varEnv.lookup(liveId.name) match {
+                morpheus.getScopeEnv(liveId).varEnv.lookup(liveId.name) match {
                     case o@One(_) => addOne(o, liveId)
                     case c@Choice(_, _, _) =>
                         // retrieve if choice is only a fake choice, caused by the parser code duplication
